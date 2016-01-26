0c2210aa027716681ebb6579110e513b8d9e75b5
Lenon Bordini <lenonbordini@live.com>
2016-01-25 23:29:21 -0200

    Commit - Organização

--- js/init.js
+++ js/init.js
@@ -1,36 +1,30 @@
 (function($){
-    $(function(){
-        $('.button-collapse').sideNav();
-        $('.parallax').parallax();
+  $(function(){
 
-        $("#githubAccount").keyup(function () {
-            if (timeout)
-                clearTimeout(timeout);
+      $("#loadClients").click(function () {
+          $("#clients").comboBox({
+              itens: [
+                  { id: 1, name: "John", age: 20 },
+                  { id: 2, name: "Steve", age: 25 },
+                  { id: 3, name: "Bill", age: 19 },
+                  { id: 4, name: "Brandon", age: 31 },
+                  { id: 5, name: "Ryan", age: 27 }
+              ],
+              value: "id",
+              text: "{id} - {name} ({age})"
+          });
+      });
 
-            var timeout = setTimeout(function () {
-                $("#repos").comboBox({
-                    url: "https://api.github.com/users/" + $("#githubAccount").val() + "/repos",
-                    value: "id",
-                    text: "{name} ({description})",
-                    onError: function (xhr) {
-                        $("#repos").html(new Option(JSON.parse(xhr.responseText).message, ""));
-                    }
-                });
-            }, 1000);
-        });
-
-        $("#loadClients").click(function () {
-            $("#clients").comboBox({
-                itens: [
-                    { id: 1, name: "John", age: 20 },
-                    { id: 2, name: "Steve", age: 25 },
-                    { id: 3, name: "Bill", age: 19 },
-                    { id: 4, name: "Brandon", age: 31 },
-                    { id: 5, name: "Ryan", age: 27 }
-                ],
-                value: "id",
-                text: "{id} - {name} ({age})"
-            });
-        });
-    });
+      var timeout;
+      $("#githubAccount").keyup(function () {
+          if (timeout) clearTimeout(timeout);
+          timeout = setTimeout(function () {
+              $("#repos").comboBox({
+                  url: "https://api.github.com/users/" + $("#githubAccount").val() + "/repos",
+                  value: "id",
+                  text: "{name} ({description})"
+              });
+          }, 1000);
+      });
+  });
 })(jQuery);
